67d8a77b3a6a5bed2d84921d347ee418247dbe80
Rivwoxx <augustocsrivera@gmail.com>
2023-05-27 03:27:39 -0600

    Day01

---
+++ PythonRe/recapmain.py
@@ -0,0 +1,22 @@
+# Strings and numbers
+# The f stands for fomart.
+print(f"The number {332} is a great number")
+print(f"The result of {22} times {3} is {22*3}")
+
+# with variables
+pepe_lota = 22 *3
+print(f"The result of {22} times {3} is {pepe_lota}")
+
+
+def time_operations(x, b):
+    print(f"The result of {x} times {b} is {x * b}")
+    
+
+time_operations(33,3)
+
+# # Calling the function before defining it
+# result = add_numbers(3, 5)
+# print(result)
+
+# def add_numbers(a, b):
+#     return a + b
